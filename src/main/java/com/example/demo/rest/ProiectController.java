4a3e9c2aa173c49ade78dea70a9e41a9bca0632d
Carolina Varzari <varzari3009@gmail.com>
2026-03-10 00:01:18 +0200

    Add endpoint

--- src/main/java/com/example/demo/rest/ProiectController.java
+++ src/main/java/com/example/demo/rest/ProiectController.java
@@ -1,4 +1,15 @@
 package com.example.demo.rest;
 
+import java.util.Scanner;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class ProiectController {
+
+    @GetMapping("/start")
+    public String nume(){
+        return "Varzari Carolina!!!!!";
+    }
+
 }
